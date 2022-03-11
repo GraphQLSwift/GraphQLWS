aa36939b8b3063b3093601dd0391a3ed8132c062
GMoseke <garrett@passivelogic.com>
2022-03-11 11:17:27 -0700

    bump nio to 2.38

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(name: "GraphQL", url: "https://github.com/GraphQLSwift/GraphQL.git", from: "2.2.1"),
         .package(name: "GraphQLRxSwift", url: "https://github.com/GraphQLSwift/GraphQLRxSwift.git", from: "0.0.4"),
         .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", from: "6.1.0"),
-        .package(name: "swift-nio", url: "https://github.com/apple/swift-nio.git", .upToNextMinor(from: "2.33.0"))
+        .package(name: "swift-nio", url: "https://github.com/apple/swift-nio.git", from: "2.33.0")
     ],
     targets: [
         .target(

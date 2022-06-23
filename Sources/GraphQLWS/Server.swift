f61ab2fc437f503baff1e03e99e655e4860c61ae
Garrett Moseke <garrett@passivelogic.com>
2022-06-23 10:45:00 -0600

    squahme more logging

--- Sources/GraphQLWS/Server.swift
+++ Sources/GraphQLWS/Server.swift
@@ -7,7 +7,7 @@ import NIO
 import RxSwift
 import HiveLogging
 
-let logger = Logger(name: "GQL-WS SERVER")
+let logger = Logger(name: "GQL_WS_SERVER")
 /// Server implements the server-side portion of the protocol, allowing a few callbacks for customization.
 public class Server {
     // We keep this weak because we strongly inject this object into the messenger callback
@@ -42,6 +42,7 @@ public class Server {
         self.onSubscribe = onSubscribe
         
         messenger.onRecieve { message in
+            logger.debug("Received message \(message)")
             guard let messenger = self.messenger else { return }
             
             self.onMessage(message)
@@ -65,7 +66,7 @@ public class Server {
                 self.error(.noType())
                 return
             }
-            
+            logger.debug("new request of type \(request.type) being processed...")
             switch request.type {
                 case .GQL_CONNECTION_INIT:
                     guard let connectionInitRequest = try? self.decoder.decode(ConnectionInitRequest.self, from: json) else {
@@ -75,6 +76,7 @@ public class Server {
                     self.onConnectionInit(connectionInitRequest, messenger)
                 case .GQL_START:
                     guard let startRequest = try? self.decoder.decode(StartRequest.self, from: json) else {
+                        logger.error("failed to decode start request")
                         self.error(.invalidRequestFormat(messageType: .GQL_START))
                         return
                     }
@@ -117,12 +119,14 @@ public class Server {
     }
     
     private func onConnectionInit(_ connectionInitRequest: ConnectionInitRequest, _ messenger: Messenger) {
+        logger.info("Connection initialized")
         guard !initialized else {
             self.error(.tooManyInitializations())
             return
         }
         
         do {
+            logger.info("Authorizing connection...")
             try self.auth(connectionInitRequest)
         }
         catch {
@@ -135,6 +139,7 @@ public class Server {
     }
     
     private func onStart(_ startRequest: StartRequest, _ messenger: Messenger) {
+        logger.info("processing start request...")
         guard initialized else {
             self.error(.notInitialized())
             return
@@ -153,6 +158,7 @@ public class Server {
         }
         
         if isStreaming {
+            logger.info("request is streaming")
             let subscribeFuture = onSubscribe(graphQLRequest)
             subscribeFuture.whenSuccess { result in
                 guard let streamOpt = result.stream else {
@@ -178,12 +184,14 @@ public class Server {
                         }
                     },
                     onError: { [weak self] error in
+                        logger.error("onError received, \(error)")
                         guard let self = self else {
                             logger.error("GQLWS WEAK SELF RESOLUTION FAIL LINE 180")
                             return }
                         self.sendError(error, id: id)
                     },
                     onCompleted: { [weak self] in
+                        logger.info("Completed observable.")
                         guard let self = self else {
                             logger.error("GQLWS WEAK SELF RESOLUTION FAIL LINE 186")
                             return }
@@ -224,6 +232,7 @@ public class Server {
     
     /// Send a `connection_ack` response through the messenger
     private func sendConnectionAck(_ payload: [String: Map]? = nil) {
+        logger.debug("Sending ack through messenger...")
         guard let messenger = messenger else { return }
         messenger.send(
             ConnectionAckResponse(payload).toJSON(encoder)
@@ -248,6 +257,8 @@ public class Server {
     
     /// Send a `data` response through the messenger
     private func sendData(_ payload: GraphQLResult? = nil, id: String) {
+        logger.debug("Sending data through messenger...")
+        logger.trace("data payload is \(payload.debugDescription)")
         guard let messenger = messenger else { return }
         messenger.send(
             DataResponse(

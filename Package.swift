39f06ca994db05cd74d1004d2681177f153d3233
Garrett Moseke <garrett@passivelogic.com>
2022-06-30 15:39:41 -0600

    feature: add async send to messenger

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "GraphQLWS",
+    platforms: [
+        .macOS(.v12),
+    ],
     products: [
         .library(
             name: "GraphQLWS",
